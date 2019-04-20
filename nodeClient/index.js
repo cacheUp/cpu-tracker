63109d444a4a6f69671780c1c84725de900dd580
cacheUp <bradb26@gmail.com>
2019-04-20 13:35:01 +0300

    set up Promise for all my os info

--- nodeClient/index.js
+++ nodeClient/index.js
@@ -1,20 +1,37 @@
 const os = require("os");
+function performanceData() {
+  return new Promise(async (res, reject) => {
+    const cpus = os.cpus();
+    const freeMem = os.freemem();
+    const totalMem = os.totalmem();
+    const osType = os.type() == "Darwin" ? "Mac" : os.type();
+    const upTime = os.uptime();
 
-const cpus = os.cpus();
-const freeMem = os.freemem();
-const totalMem = os.totalmem();
-const osType = os.type() == "Darwin" ? "Mac" : os.type();
-const upTime = os.uptime();
+    const usedMem = totalMem - freeMem;
 
-const usedMem = totalMem - freeMem;
+    const memUsage = Math.floor(((usedMem / totalMem) * 100) / 100);
 
-const memUsage = Math.floor(((usedMem / totalMem) * 100) / 100);
-
-const cpuModel = cpus[0].model;
-const numCores = cpus.length;
-const cpuSpeed = cpus[0].speed;
+    const cpuModel = cpus[0].model;
+    const numCores = cpus.length;
+    const cpuSpeed = cpus[0].speed;
+    const cpuLoad = await getCpuLoad();
+    res({
+      freeMem,
+      totalMem,
+      usedMem,
+      memUsage,
+      osType,
+      upTime,
+      cpuModel,
+      numCores,
+      cpuSpeed,
+      cpuLoad
+    });
+  });
+}
 
 function cpuAverage() {
+  const cpus = os.cpus();
   let idleMs = 0;
   let totalMs = 0;
   cpus.forEach(aCore => {
@@ -29,4 +46,20 @@ function cpuAverage() {
   };
 }
 
-console.log(cpuAverage());
+function getCpuLoad() {
+  return new Promise((res, rej) => {
+    const start = cpuAverage();
+    setTimeout(() => {
+      const end = cpuAverage();
+      const idleDifference = end.idle - start.idle;
+      const totalDifference = end.total - start.total;
+      const percentageCpu =
+        100 - Math.floor((100 * idleDifference) / totalDifference);
+      res(percentageCpu);
+    }, 100);
+  });
+}
+
+performanceData().then(data => {
+  console.log(data);
+});
