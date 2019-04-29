df2dfd22a7bc1a17ec133bf74ceab862cfc187a0
cacheUp <bradb26@gmail.com>
2019-04-29 16:37:47 +0300

    canvas circle displayed for cpuLoad

--- react-client/src/Cpu.js
+++ react-client/src/Cpu.js
@@ -2,11 +2,15 @@ import React from "react";
 import drawCircle from "./utilities/canvasLoadAnimation";
 
 function Cpu(props) {
+  const canvas = document.querySelector("canvas");
+  drawCircle(canvas, props.cpuLoad);
   console.log(props);
   return (
     <div className="col-sm-3 cpu">
       <h3>Cpu Load</h3>
-      <div className="canvas-wrapper" />
+      <div className="canvas-wrapper">
+        <canvas className="canvas" />
+      </div>
     </div>
   );
 }
