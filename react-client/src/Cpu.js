bf5d6a4a257b708d55ed8c7c1010eb7d66daa494
cacheUp <bradb26@gmail.com>
2019-04-29 16:33:11 +0300

    Cpu canvas start

--- react-client/src/Cpu.js
+++ react-client/src/Cpu.js
@@ -1,7 +1,14 @@
 import React from "react";
+import drawCircle from "./utilities/canvasLoadAnimation";
 
 function Cpu(props) {
-  return <h3>Cpu!</h3>;
+  console.log(props);
+  return (
+    <div className="col-sm-3 cpu">
+      <h3>Cpu Load</h3>
+      <div className="canvas-wrapper" />
+    </div>
+  );
 }
 
 export default Cpu;
