132e17ee137daf39f1f976b303a81de9f8533de8
cacheUp <bradb26@gmail.com>
2019-05-02 16:57:04 +0300

    Mem canvas setup

--- react-client/src/Mem.js
+++ react-client/src/Mem.js
@@ -1,7 +1,16 @@
 import React from "react";
+import drawCircle from "./utilities/canvasLoadAnimation";
 
 function Mem(props) {
-  return <h3>mem!</h3>;
+  const { totalMem, usedMem, memUsage, freeMem } = props.memData;
+  return (
+    <div className="col-sm-3 mem">
+      <h3>mem!</h3>
+      <div className="canvas-wrapper">
+        <canvas className="canvas" width="200" height="200" />
+      </div>
+    </div>
+  );
 }
 
 export default Mem;
