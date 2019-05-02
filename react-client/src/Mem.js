008c26b9085121e0ffbcbe95d725eaf5af9c023d
cacheUp <bradb26@gmail.com>
2019-05-02 17:07:10 +0300

    active bar for Mem

--- react-client/src/Mem.js
+++ react-client/src/Mem.js
@@ -3,11 +3,14 @@ import drawCircle from "./utilities/canvasLoadAnimation";
 
 function Mem(props) {
   const { totalMem, usedMem, memUsage, freeMem } = props.memData;
+  const canvas = document.querySelector(".memCanvas");
+  drawCircle(canvas, memUsage * 100);
+  console.log(memUsage);
   return (
     <div className="col-sm-3 mem">
-      <h3>mem!</h3>
+      <h3>medm!</h3>
       <div className="canvas-wrapper">
-        <canvas className="canvas" width="200" height="200" />
+        <canvas className="memCanvas" width="200" height="200" />
       </div>
     </div>
   );
