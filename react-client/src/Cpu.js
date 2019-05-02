7eed80d87f7399ecd2e5265a4e9ca69f5727bf6e
cacheUp <bradb26@gmail.com>
2019-05-02 16:43:57 +0300

    corresponding number to active bar

--- react-client/src/Cpu.js
+++ react-client/src/Cpu.js
@@ -10,6 +10,7 @@ function Cpu(props) {
       <h3>Cpu Load</h3>
       <div className="canvas-wrapper">
         <canvas className="canvas" />
+        <div className="cpu-text">{props.cpuData.cpuLoad}</div>
       </div>
     </div>
   );
